a70a7db3dc9c53abf4c81df38660df9227e77514
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2020-02-13 00:36:49 +0900

    カンマ区切りで配列に入れ，改行で次の要素に切り替える処理

--- OekakiModel.js
+++ OekakiModel.js
@@ -3,8 +3,9 @@
 //==========================================
 var lineNum;
 var columnNum;
-var lineInputData = "";
-var columnInputData = "";
+// 2次元配列
+var lineArray = [];
+var columnArray = [];
 
 //==========================================
 // 更新時にテキストエリアを初期化
@@ -18,20 +19,82 @@ $(function() {
 });
 
 function lineInputButton() {
-	this.lineInputData = document.form1.lineInputTextArea.value; 
+	var lineDataString = document.form1.lineInputTextArea.value;
+
+	// 2次元配列にするために必要
+	var array = [];
+	for (var k = 0; k < lineDataString.length; k++) {
+		console.log("lineDataString[" + k + "]" + lineDataString[k]);
+		if (lineDataString[k] != '\n') {
+			// ，なら見逃し
+			if (lineDataString[k] != ',') {
+				// 2桁数字かどうか
+				if ((k + 1) < lineDataString.length
+				 && lineDataString[k+1] != ',') {
+					var st = (lineDataString[k] + lineDataString[k+1]);
+					array.push(st);
+					k++;
+				} else {
+					array.push(lineDataString[k]);
+				}
+			}
+		} else {
+			console.log("OK");
+			this.lineArray.push(array);
+			array = [];
+		}
+		if (k == lineDataString.length - 1) {
+			this.lineArray.push(array);
+		}
+	}
+	for (var i = 0; i < 2; i++) {
+		for (var j = 0; j < 10; j++) {
+			console.log("lineArray[" + i + "][" + j + "] = " + this.lineArray[i][j]);
+		}
+	}
 }
 
 function lineDeleteButton() {
-	this.lineInputData = "";
+	this.lineArray = [];
 	document.form1.lineInputTextArea.value = "";
 }
 
 function columnInputButton() {
-	this.columnInputData = document.form2.columnInputTextArea.value;
+	var columnDataString = document.form2.columnInputTextArea.value;
+
+	// 2次元配列にするために必要
+	var array = [];
+	for (var k = 0; k < columnDataString.length; k++) {
+		if (columnDataString[k] != '\n') {
+			// ，なら見逃し
+			if (columnDataString[k] != ',') {
+				// 2桁数字かどうか
+				if ((k + 1) < columnDataString.length
+				 && columnDataString[k+1] != ',') {
+					var st = (columnDataString[k] + columnDataString[k+1]);
+					array.push(st);
+					k++;
+				} else {
+					array.push(columnDataString[k]);
+				}
+			}
+		} else {
+			this.columnArray.push(array);
+			array = [];
+		}
+		if (k == columnDataString.length - 1) {
+			this.columnArray.push(array);
+		}
+	}
+	for (var i = 0; i < 2; i++) {
+		for (var j = 0; j < 10; j++) {
+			console.log("columnArray[" + i + "][" + j + "] = " + this.columnArray[i][j]);
+		}
+	}
 }
 
 function columnDeleteButton() {
-	this.columnInputData = "";
+	this.columnArray = [];
 	document.form2.columnInputTextArea.value = "";
 }
 
@@ -41,9 +104,6 @@ function showAnswerButton() {
 
 
 	// 表を作成
-	console.log("lineInputData = " + typeof(this.lineInputData));
-	console.log("columnInputData = " + typeof(this.columnInputData));
-	
 	
 	var tableArea = document.getElementById("showTable");
 	var createTable = document.createElement('table');
