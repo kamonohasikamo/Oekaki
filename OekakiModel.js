bff23614ccde8a1af264176d503522e2a5188525
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2020-02-12 23:27:33 +0900

    列と行の入力欄を作成

--- OekakiModel.js
+++ OekakiModel.js
@@ -1,7 +1,10 @@
 //==========================================
 // 変数宣言
 //==========================================
-
+var lineNum;
+var columnNum;
+var lineInputData = "";
+var columnInputData = "";
 
 //==========================================
 // 更新時にテキストエリアを初期化
@@ -14,11 +17,34 @@ $(function() {
 	columnCountText.value = "";
 });
 
-function clickButton() {
-	var lineNum = document.getElementById("inputLineCountId").value;
-	var columnNum = document.getElementById("inputColumnCountId").value;
+function lineInputButton() {
+	this.lineInputData = document.form1.lineInputTextArea.value; 
+}
+
+function lineDeleteButton() {
+	this.lineInputData = "";
+	document.form1.lineInputTextArea.value = "";
+}
+
+function columnInputButton() {
+	this.columnInputData = document.form2.columnInputTextArea.value;
+}
+
+function columnDeleteButton() {
+	this.columnInputData = "";
+	document.form2.columnInputTextArea.value = "";
+}
+
+function showAnswerButton() {
+	this.lineNum = document.inputForm.lineCount.value;
+	this.columnNum = document.inputForm.columnCount.value;
+
 
 	// 表を作成
+	console.log("lineInputData = " + typeof(this.lineInputData));
+	console.log("columnInputData = " + typeof(this.columnInputData));
+	
+	
 	var tableArea = document.getElementById("showTable");
 	var createTable = document.createElement('table');
 	createTable.id = "showTableId";
@@ -26,13 +52,13 @@ function clickButton() {
 
 	var createTableBody = document.createElement('tbody');
 	createTable.appendChild(createTableBody);
-	if(lineNum != null 
-		&& lineNum > 0
-		&& columnNum != null
-		&& columnNum > 0) {
-		for (var i = 0; i < lineNum; i++) {
+	if(this.lineNum != null 
+		&& this.lineNum > 0
+		&& this.columnNum != null
+		&& this.columnNum > 0) {
+		for (var i = 0; i < this.lineNum; i++) {
 			var tr = document.createElement('tr');
-			for (var j = 0; j < columnNum; j++) {
+			for (var j = 0; j < this.columnNum; j++) {
 				var td = document.createElement('td');
 				td.appendChild(document.createTextNode(j));
 				createTableBody.appendChild(td);
